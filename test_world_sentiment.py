84bd3d3bcab99a85e52655b6a98b84e9d1680c0f
Farhan Tanvir Khan <90358727+ftkovr9k@users.noreply.github.com>
2021-11-09 00:42:00 -0500

    Update test_world_sentiment.py

--- test_world_sentiment.py
+++ test_world_sentiment.py
@@ -1,5 +1,14 @@
 import world_sentiment as world
 from pytest import approx
 
-def test:
-  pass
+def check_avg_score:
+  assert world.generate_average_sentiment_score == average_score;
+def check_score:
+  assert world.first_score == world.generate_average_sentiment_score(first_thing)
+  assert world.second_score == world.generate_average_sentiment_score(second_thing)
+
+ def check_greater:
+  assert world.if(first_score>second_score):
+      print(f"The humanity prefers {first_thing} over {second_thing}")
+ else:
+      print(f"The humanity prefers {second_thing} over {first_thing}")
